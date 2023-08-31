111518382d8445b172855562219e068005ba5b5c
Amit Tambulkar <amittambulkar104@gmail.com>
2023-08-31 17:24:46 +0530

    Updated to scrape data from 2 webpages

--- webscrapper.py
+++ webscrapper.py
@@ -5,33 +5,45 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 
 
-driver = webdriver.Chrome()
-driver.get("http://quotes.toscrape.com/")
+driver1 = webdriver.Chrome()
+driver2 = webdriver.Chrome()
+driver1.get('http://quotes.toscrape.com/')
+driver2.get('http://quotes.toscrape.com/page/2/')
 
-
-scopes = [
-    'http://www.googleapis.com/auth/spreadsheets'
-    'http://www.googleapis.com/auth/drive'
+scopes =[
+    "http://www.googleapis.com/spreadsheets"
+    "http://www.googleapis.com/drive"
 ]
 
 Credentials = ServiceAccountCredentials.from_json_keyfile_name('auth.json')
 file = gspread.authorize(credentials=Credentials)
-sheet = file.open("Web_Scrapper")
+sheet = file.open('Web_Scrapper')
 
 sheet = sheet.sheet1
 
+for i in range(1, 10):
+    quote1 = driver1.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/span[1]').text
+    quote_by1 = driver1.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/span[2]/small').text
+    tags1 = driver1.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/div').text
+    
+    print(quote1)
+    print(quote_by1)
+    print(tags1)
 
+    print('')
 
-for i in range(1, 9):
-    Quote = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div["+str(i)+"]/span[1]").text
-    Quote_by = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div["+str(i)+"]/span[2]/small").text
-    tags = driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/div["+str(i)+"]/div").text
+    quote2 = driver2.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/span[1]').text
+    quote_by2 = driver2.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/span[2]/small').text
+    tags2 = driver2.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div['+str(i)+']/div').text
 
-    print(Quote)
-    print(Quote_by)
-    print(tags)
+    print(quote2)
+    print(quote_by2)
+    print(tags2)
 
+    sheet.update_acell('A'+str(i+1),quote1)
+    sheet.update_acell('B'+str(i+1),quote_by1)
+    sheet.update_acell('C'+str(i+1),tags1)
 
-    sheet.update_acell('A'+str(i+1), Quote)
-    sheet.update_acell('B'+str(i+1), Quote_by)
-    sheet.update_acell('C'+str(i+1), tags)
+    sheet.update_acell('A1'+str(i+1),quote2)
+    sheet.update_acell('B1'+str(i+1),quote_by2)
+    sheet.update_acell('C1'+str(i+1),tags2)
